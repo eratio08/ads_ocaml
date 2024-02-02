804c0282123a621da6f03cc4391274a4c2afe11f
Eike Lurz <eike.lurz@tuta.io>
2024-02-02 21:46:46 +0100

    Another type for bin tree

--- lib/tree.ml
+++ lib/tree.ml
@@ -1,5 +1,16 @@
 module Tree (V : Ord.Ord) = struct
+  type e = V.t
+
   type t =
     | Empty
-    | Cons of t * V.t * t
+    | Cons of t * e * t
+end
+
+(* TODO implement conversion from splay heap to bin tree *)
+module BinTree (Elem : Ord.Ord) = struct
+  type e = Elem.t
+
+  type t =
+    | E
+    | T of e * t * t
 end
