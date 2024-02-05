4621cc8004bff26e90288c4c03104d9b2a620b1d
Eike Lurz <eike.lurz@tuta.io>
2024-02-05 12:48:06 +0100

    Add another lazy queue

--- lib/queue.ml
+++ lib/queue.ml
@@ -119,7 +119,8 @@ module FiFoDequeue = struct
   let init_exn t = init t |> Option.get
 end
 
-(* Lazy Queue that has an amortized run time of O(1) for all operations. *)
+(* Lazy Queue that has an amortized run time of O(1) for all operations.
+   Using incremental suspension, by utilizing streams. *)
 module StreamQueue = struct
   open Stream
 
@@ -151,6 +152,58 @@ module StreamQueue = struct
   let tail = function
     | _, Stream.Nil, _, _ -> None
     | len_f, Stream.Cons (lazy (_, f)), len_r, r -> Some (check (len_f - 1, f, len_r, r))
+  ;;
+
+  let tail_exn t = tail t |> Option.get
+end
+
+(** Lazy Queue, using monolithic suspension. *)
+module LazyQueue = struct
+  (** The lazy queue consists of:
+      1) a working copy of the suspended front list
+      2) the size of the front list
+      3) the suspended front of the list
+      4) the size of the rear list
+      5) the rear list *)
+  type 'a t = 'a list * int * 'a list Stdlib.Lazy.t * int * 'a list
+
+  (** Check if the working copy needs to be set.
+      If the working copy is empty evaluate the front list and make it the working copy. *)
+  let check_w = function
+    (* use force here to not evaluate while matching the pattern *)
+    | [], len_f, f, len_r, r -> Stdlib.Lazy.force f, len_f, f, len_r, r
+    | q -> q
+  ;;
+
+  (** Check if the invariant that front list is always at least as long as the rear list.
+      If the rear list is shorter or equal to the front list, check if the working copy need to be created.
+      If the rear list is longer, lazy reverse the rear list and append it to the front list,
+      the rear list is empty now. *)
+  let check = function
+    | (_, len_f, _, len_r, _) as q when len_r <= len_f -> check_w q
+    (* len_r > lenf_f *)
+    | _, len_f, (lazy f), len_r, r ->
+      check_w (f, len_f + len_r, lazy (f @ List.rev r), 0, [])
+  ;;
+
+  (** Append to the rear. *)
+  let snoc x (w, len_f, f, len_r, r) = check (w, len_f, f, len_r + 1, x :: r)
+
+  (** Return the head of the queue.
+      Using the head of the working copy prefix. *)
+  let head = function
+    | [], _, _, _, _ -> None
+    | x :: _, _, _, _, _ -> Some x
+  ;;
+
+  let head_exn t = head t |> Option.get
+
+  (** Return the tail of the queue.
+      Removes the head of the working copy and lazily tails the front queue and reduces the tracked length by one. *)
+  let tail = function
+    | [], _, _, _, _ -> None
+    | _ :: w, len_f, (lazy f), len_r, r ->
+      Some (check (w, len_f - 1, lazy (List.tl f), len_r, r))
   ;;
 
   let tail_exn t = tail t |> Option.get
